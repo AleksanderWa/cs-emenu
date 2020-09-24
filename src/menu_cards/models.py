c27e93a14b1da7908960c082920694cc0cf12692
Aleksander <walkowski.aleksander@gmail.com>
2020-09-24 09:30:20 +0200

    use TimestampedModel for time management

--- src/menu_cards/models.py
+++ src/menu_cards/models.py
@@ -1,19 +1,14 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
 FOOD_TYPE_CHOICES = ((10, "Meat"), (11, "Vegetarian"), (12, "Vegan"))
 
 
-class TimeManager(models.Model):
-    creation_date = models.TimeField(auto_now=True)
-    last_update = models.TimeField(default=creation_date)
-
-
-class Dish(models.Model):
+class Dish(TimeStampedModel):
     name = models.CharField(max_length=100, unique=True)
     description = models.CharField(default="", max_length=250)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     prep_time = models.DurationField()
-    time_stamps = models.ForeignKey(TimeManager, on_delete=models.CASCADE, null=True)
     food_type = models.SmallIntegerField(
         choices=FOOD_TYPE_CHOICES, default=10, blank=True, help_text="Type of food"
     )
@@ -22,8 +17,7 @@ class Dish(models.Model):
         return self.name
 
 
-class MenuCard(models.Model):
+class MenuCard(TimeStampedModel):
     name = models.CharField(max_length=100, unique=True)
     description = models.CharField(default="", max_length=250)
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE, null=True)
-    time_stamps = models.ForeignKey(TimeManager, on_delete=models.CASCADE, null=True)
