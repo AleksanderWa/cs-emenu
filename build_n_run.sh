a26032445727bd366362c8ae4eed753046ece98d
Aleksander <walkowski.aleksander@gmail.com>
2020-10-02 17:45:52 +0200

    fixed no input

--- build_n_run.sh
+++ build_n_run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+set -x
+
 docker-compose up --build -d
-winpty docker-compose exec web python ./manage.py flush
-winpty docker-compose exec web python ./manage.py migrate --no-input
+winpty docker-compose exec web python ./manage.py flush --no-input
+winpty docker-compose exec web python ./manage.py migrate
 winpty docker-compose exec web python ./manage.py seed_db
 winpty docker-compose exec web python ./manage.py create_users
 
