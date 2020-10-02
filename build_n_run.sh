b6794335473e7a79acff975ce0d144ea92b3cdf7
Aleksander <walkowski.aleksander@gmail.com>
2020-10-02 17:40:00 +0200

    update build script

--- build_n_run.sh
+++ build_n_run.sh
@@ -4,3 +4,5 @@ docker-compose up --build -d
 winpty docker-compose exec web python ./manage.py flush
 winpty docker-compose exec web python ./manage.py migrate --no-input
 winpty docker-compose exec web python ./manage.py seed_db
+winpty docker-compose exec web python ./manage.py create_users
+
